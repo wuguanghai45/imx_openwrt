656ebd17497e5c09583e1c77ddbd1325a772e864
hzx <1289514315@qq.com>
2025-12-22 02:48:17 +0000

    Add boot copy scripts for i.MX9352 board
    This commit introduces two new shell scripts, `copy_boot.sh` and `copy.sh`, to facilitate the copying of boot and filesystem images for the i.MX9352 board. The scripts automate the process of transferring the necessary binary files to the designated boot media, enhancing the setup process for the board.
    
    Signed-off-by: [Your Name] <your.email@example.com>
    Signed-off-by: hzx <1289514315@qq.com>

---
+++ copy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+cp bin/targets/imx/cortexa55/openwrt-imx-cortexa55-imx9352-squashfs-boot.bin /media/wgh/boot/ok-mx93-uboot.bin
+cp bin/targets/imx/cortexa55/openwrt-imx-cortexa55-imx9352-squashfs-sdcard.img /media/wgh/boot/ok-mx93-linux-fs.sdcard.aa
---
+++ copy_boot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+cp bin/targets/imx/cortexa55/openwrt-imx-cortexa55-imx9352-squashfs-boot.bin /media/wgh/boot/ok-mx93-uboot.bin
